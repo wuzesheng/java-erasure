66edcf2ee3522762a637154f72c8683633f1dd7e
wuzesheng <wuzesheng@xiaomi.com>
2014-07-03 12:26:57 +0800

    Initialized maven project

---
+++ src/main/java/com/xiaomi/infra/App.java
@@ -0,0 +1,13 @@
+package com.xiaomi.infra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
